5a5ae5d36e6fb9d3e4178b8613e564a188eb7ee6
Matthew Mitchener <mattjmitchener@gmail.com>
2026-02-27 20:41:21 -0800

    fix: replace axis gizmo with GizmoHelper fixed to top-left corner

--- src/components/viewport/AxisGizmo.tsx
+++ src/components/viewport/AxisGizmo.tsx
@@ -1,22 +1,12 @@
-import { useMemo } from 'react';
-import { Html } from '@react-three/drei';
-import { AxesHelper } from 'three';
+import { GizmoHelper, GizmoViewport } from '@react-three/drei';
 
 export function AxisGizmo() {
-  const axesHelper = useMemo(() => new AxesHelper(1), []);
-
   return (
-    <group position={[0, 0.01, 0]}>
-      <primitive object={axesHelper} />
-      <Html position={[1.15, 0, 0]} center>
-        <span style={{ color: '#ef4444', fontSize: '11px', fontWeight: 'bold', userSelect: 'none', textShadow: '0 0 3px white' }}>X</span>
-      </Html>
-      <Html position={[0, 1.15, 0]} center>
-        <span style={{ color: '#22c55e', fontSize: '11px', fontWeight: 'bold', userSelect: 'none', textShadow: '0 0 3px white' }}>Y</span>
-      </Html>
-      <Html position={[0, 0, 1.15]} center>
-        <span style={{ color: '#3b82f6', fontSize: '11px', fontWeight: 'bold', userSelect: 'none', textShadow: '0 0 3px white' }}>Z</span>
-      </Html>
-    </group>
+    <GizmoHelper alignment="top-left" margin={[80, 80]}>
+      <GizmoViewport
+        axisColors={['#ef4444', '#22c55e', '#3b82f6']}
+        labelColor="white"
+      />
+    </GizmoHelper>
   );
 }
